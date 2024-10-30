d5b321c926ad0a05da604c1d840e9df46ccf6f0c
Huma-Mohsin <160951214+Huma-Mohsin@users.noreply.github.com>
2024-10-30 11:28:41 +0500

    code updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,29 +9,23 @@ export default function Home() {
   let count = 0;
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <div>
-        <br />
-        <br />
-        <br />
+      
         <h1>Learning Click Event At Button</h1>
-        <br />
-        <br />
+        
         <h1>CountNumber: {count}</h1>
-        <br />
-        <br />
+        
         <button
           className="bg-blue-800 p-10 text-lg cursor-pointer text-white"
           onClick={() => {
-            count++;
+            count++;//state donot change at localhost but change in browser site
             console.log("Button clicked", count);
           }}
         >
           I am a click Event on this Button
-        </button>//Result can be shown at Browser's console,right click on localhost,inspect and then console.
+        </button>
       </div>
       
-    </div>
   );
 }
-
+//Result can be shown at Browser's console,right click on localhost,inspect and then console.
 //onclick is an event and code inside onclick is executed after button being clicked.
